24f0638030665dbc5b22aa562848bdc996e961b1
duechayapolgmit <duechayapolsbs@gmail.com>
2025-06-24 00:34:30 +0700

    feat: model head

--- scripts/ml.js
+++ scripts/ml.js
@@ -6,7 +6,7 @@ import '@tensorflow/tfjs-react-native';
 const MOBILE_NET_INPUT_WIDTH = 224;
 const MOBILE_NET_INPUT_HEIGHT = 224;
 const STOP_DATA_GATHER = -1;
-const CLASS_NAMES = [];
+const CLASS_NAMES = ["0", "1"];
 
 // Variables
 let mobilenet = undefined;
@@ -31,3 +31,21 @@ export async function loadMobileNetFeatureModel() {
   });
   return 'MobileNet v3 loaded successfully!';
 }
+
+// Model head
+let model = tf.sequential();
+model.add(tf.layers.dense({inputShape: [1024], units: 128, activation: 'relu'}));
+model.add(tf.layers.dense({units: CLASS_NAMES.length, activation: 'softmax'}));
+
+model.summary();
+
+// Compile the model with the defined optimizer and specify a loss function to use.
+model.compile({
+  // Adam changes the learning rate over time which is useful.
+  optimizer: 'adam',
+  // Use the correct loss function. If 2 classes of data, must use binaryCrossentropy.
+  // Else categoricalCrossentropy is used if more than 2 classes.
+  loss: (CLASS_NAMES.length === 2) ? 'binaryCrossentropy': 'categoricalCrossentropy', 
+  // As this is a classification problem you can record accuracy in the logs too!
+  metrics: ['accuracy']  
+});
