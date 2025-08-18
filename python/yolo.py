77e6581d08ef24772e3044ae9e0bdb1e61738206
duechayapolgmit <duechayapolsbs@gmail.com>
2025-08-19 00:41:20 +0100

    direction tracking for yolo

--- python/yolo.py
+++ python/yolo.py
@@ -5,18 +5,19 @@ import requests
 
 # Params
 URL = 'http://localhost:5000/api/data'
+THRESHOLD = 5
 
 # Class for the objects:
 class Obj:
-    def __init__(self, id, timestamp, cls, direction):
+    def __init__(self, id, timestamp, cls, direction, centre):
         self.id = id
         self.cls = cls
         self.direction = direction
         self.timestamp = timestamp
+        self.centre = centre
 
 # Function to send data to the server
 def send_data(obj):
-    
     # Data for Server - all
     g = geocoder.ip('me')
     latitude = g.latlng[0]
@@ -45,33 +46,42 @@ results = model.track(source=0, show= True, tracker="bytetrack.yaml", conf=0.5, 
 
 # Tracking and results
 for result in results:
+    # Current frame data
     curr_ids = result.boxes.id
     curr_classes = result.boxes.cls
+    curr_boxes = result.boxes.xywh
     curr_objs = []
 
+    # Previous frame data
+    prev_objs_dict = {obj.id: obj for obj in prev_objs}
+
     # Check the current frame
-    if curr_ids is None:    # If current frame doesn't have any objects, assumed that all the previous objects are gone
+    if curr_ids is None: # If current frame doesn't have any objects, assumed that all the previous objects are gone
         for prev_obj in prev_objs:
             send_data(prev_obj)
-    else:                   # If current frame has an object, compare with the previous one (via ID) and pop out the one that doesn't match
+    else: # If current frame has an object, compare with the previous one (via ID) and pop out the one that doesn't match
         for i in range(len(curr_ids)):
-            # Detect if same as previous one or not
-            same = False
-            for prev_obj in prev_objs:
-                if prev_obj.id == curr_ids[i]:
-                    curr_objs.append(prev_obj)
-                    same = True
-                    break
-            if same:
-                continue
+            # Get current object's data
+            curr_id = int(curr_ids[i])
+            curr_centre = curr_boxes[i][0].item()
+            curr_cls = int(curr_classes[i])
 
-            # If it's not the same object -> it's a new one, so create a new object
-            id = int(curr_ids[i])
-            cls = int(curr_classes[i])
-            timestamp = int(time.time()*1000)
-            direction = 'LEFT' # for now - TODO: implement direction bits
-            new_obj = Obj(id, timestamp, cls, direction)
-            curr_objs.append(new_obj)
+            # Detect if same as one of the previous ones or not - if yes, check direction and update
+            if curr_id in prev_objs_dict:
+                prev_obj = prev_objs_dict[curr_id]
+
+                # Get direction
+                if curr_centre > prev_obj.centre + THRESHOLD:
+                    prev_obj.direction = 'RIGHT'
+                elif curr_centre < prev_obj.centre - THRESHOLD:
+                    prev_obj.direction = 'LEFT'
+
+                # Update
+                prev_obj.centre = curr_centre
+                curr_objs.append(prev_obj)
+            else: # If it's not the same object -> it's a new one, so create a new object
+                new_obj = Obj(curr_id, timestamp=int(time.time()*1000), cls=curr_cls, direction='STILL', centre=curr_centre)
+                curr_objs.append(new_obj)
 
     # Check the previous frame
     if prev_objs:
