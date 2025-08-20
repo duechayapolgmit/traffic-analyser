2b3405d2df570fc5aff7b97b2560ceffbca3ab04
duechayapolgmit <duechayapolsbs@gmail.com>
2025-08-20 20:54:51 +0100

    same id frame fix - via frame grace period + add id to the category

--- python/yolo.py
+++ python/yolo.py
@@ -5,7 +5,8 @@ import requests
 
 # Params
 URL = 'http://localhost:5000/api/data'
-THRESHOLD = 5
+DIRECTION_THRESHOLD = 5
+FRAME_THRESHOLD = 10 # grace period
 
 # Class for the objects:
 class Obj:
@@ -15,6 +16,7 @@ class Obj:
         self.direction = direction
         self.timestamp = timestamp
         self.centre = centre
+        self.grace = 0
 
 # Function to send data to the server
 def send_data(obj):
@@ -24,18 +26,16 @@ def send_data(obj):
     longitude = g.latlng[1]
 
     # Data for Server - individual
-    class_name = model.names[int(obj.cls)]
+    class_name = '{} ({})'.format(model.names[int(obj.cls)], obj.id)
     obj_to_send = {'latitude': latitude, 
                     'longitude': longitude, 
                     'category': class_name, 
                     'timestamp': obj.timestamp, 
                     'direction': obj.direction}
-    
-    print(obj_to_send)
 
     # Send to Server
     requests.post(URL, json=obj_to_send)
-    print('New object sent: ', obj.id, obj.timestamp, class_name, latitude, longitude)
+    print('New object sent: ', class_name, obj.timestamp, latitude, longitude)
 
 # Load a model
 model = YOLO("yolo11n.pt")
@@ -56,10 +56,7 @@ for result in results:
     prev_objs_dict = {obj.id: obj for obj in prev_objs}
 
     # Check the current frame
-    if curr_ids is None: # If current frame doesn't have any objects, assumed that all the previous objects are gone
-        for prev_obj in prev_objs:
-            send_data(prev_obj)
-    else: # If current frame has an object, compare with the previous one (via ID) and pop out the one that doesn't match
+    if curr_ids is not None: # If current frame doesn't have any objects, assumed that all the previous objects are gone
         for i in range(len(curr_ids)):
             # Get current object's data
             curr_id = int(curr_ids[i])
@@ -71,19 +68,21 @@ for result in results:
                 prev_obj = prev_objs_dict[curr_id]
 
                 # Get direction
-                if curr_centre > prev_obj.centre + THRESHOLD:
+                if curr_centre > prev_obj.centre + DIRECTION_THRESHOLD:
                     prev_obj.direction = 'RIGHT'
-                elif curr_centre < prev_obj.centre - THRESHOLD:
+                elif curr_centre < prev_obj.centre - DIRECTION_THRESHOLD:
                     prev_obj.direction = 'LEFT'
 
                 # Update
                 prev_obj.centre = curr_centre
+                prev_obj.grace = 0
                 curr_objs.append(prev_obj)
             else: # If it's not the same object -> it's a new one, so create a new object
                 new_obj = Obj(curr_id, timestamp=int(time.time()*1000), cls=curr_cls, direction='STILL', centre=curr_centre)
                 curr_objs.append(new_obj)
 
     # Check the previous frame
+    add_back = []
     if prev_objs:
         for prev_obj in prev_objs:
             exist = False
@@ -92,6 +91,14 @@ for result in results:
                     exist = True
                     break
             if not exist:
-                send_data(prev_obj) # If doesn't exist in the current frame anymore - send the data
+                if prev_obj.grace <= FRAME_THRESHOLD:
+                    prev_obj.grace = prev_obj.grace + 1
+                    add_back.append(prev_obj)
+                else:
+                    send_data(prev_obj) # If doesn't exist in the current frame anymore and passed the grace period - send the data
+    
+    # Add back the previous frame object - if any exists
+    for ab in add_back:
+        curr_objs.append(ab)
 
     prev_objs = curr_objs.copy()
